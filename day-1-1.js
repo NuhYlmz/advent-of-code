493e70dbf440930099f78a2afc490b35b31f6005
NuhYlmz <senturknuhyilmaz@gmail.com>
2023-12-07 13:36:33 +0300

    first commit

---
+++ day-1-1.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const lines = []
+fs.readFileSync('day-1/input.txt', 'utf-8')
+  .split(/\r?\n/)
+  .forEach(function (line) {
+    lines.push(line)
+  })
+
+  const results = []
+  let total= 0
+lines.forEach((line) => {
+  let firstNumber, lastNumber
+  line.split('').forEach((char) => {
+    if (!isNaN(parseFloat(char))){
+      lastNumber = char
+      if(!firstNumber){
+        firstNumber = char
+      }
+    }
+  })
+  const result = parseInt(`${firstNumber}${lastNumber}`)
+  results.push(result)
+  total+= result
+})
+
+console.log(results)
+console.log(total)
+
